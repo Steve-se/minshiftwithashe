bb38ab8f495b36f5e2446e322ad0cef362adac87
Big Steve <ejemstephen@gmail.com>
2025-01-16 04:00:25 +0100

    changed debug to false

--- mindshift/settings.py
+++ mindshift/settings.py
@@ -16,7 +16,7 @@ environ.Env.read_env(os.path.join('.env'))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
 ALLOWED_HOSTS = ['*']
 
