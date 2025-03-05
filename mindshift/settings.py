f1ffecad2e49076054f53b5865df0d2e88ce97d0
Big Steve <ejemstephen@gmail.com>
2025-03-05 19:16:44 +0100

    changed allowed hsot

--- mindshift/settings.py
+++ mindshift/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhos:8000', 'wwww.mindshiftwithashe.com', 'mindshiftwithashe.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
